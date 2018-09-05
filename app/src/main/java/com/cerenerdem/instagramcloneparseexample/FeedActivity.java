e4397cd94f01b80b6e7bbf07a4c4f93e2478acbf
ceren <cerdem@geobilgi.com>
2018-09-05 16:41:53 +0300

    İyileştirildi.

--- app/src/main/java/com/cerenerdem/instagramcloneparseexample/FeedActivity.java
+++ app/src/main/java/com/cerenerdem/instagramcloneparseexample/FeedActivity.java
@@ -1,24 +1,43 @@
 package com.cerenerdem.instagramcloneparseexample;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeedActivity extends AppCompatActivity {
+
+    ListView liste;
+    ArrayList<String> usernamesFromParse;
+    ArrayList<String> usercommentsFromParse;
+    ArrayList<Bitmap> userimageFromParse;
+    PostClass postClass;
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu,menu);
+        menuInflater.inflate(R.menu.main_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
 
@@ -28,20 +47,20 @@ public class FeedActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == R.id.add_post){
+        if (item.getItemId() == R.id.add_post) {
 
-            Intent intent = new Intent(getApplicationContext(),UploadActivity.class);
+            Intent intent = new Intent(getApplicationContext(), UploadActivity.class);
             startActivity(intent);
 
-        } else if (item.getItemId() == R.id.logout){
+        } else if (item.getItemId() == R.id.logout) {
 
             ParseUser.logOutInBackground(new LogOutCallback() {
                 @Override
                 public void done(ParseException e) {
-                    if (e != null){
-                        Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                    if (e != null) {
+                        Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
                     } else {
-                        Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                        Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
                         startActivity(intent);
                     }
                 }
@@ -56,5 +75,60 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+
+        liste = (ListView) findViewById(R.id.liste);
+
+        usernamesFromParse = new ArrayList<>();
+        usercommentsFromParse = new ArrayList<>();
+        userimageFromParse = new ArrayList<>();
+
+        postClass = new PostClass(usernamesFromParse, usercommentsFromParse, userimageFromParse, this);
+
+        liste.setAdapter(postClass);
+
+        indir();
     }
+
+
+    public void indir() {
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Posts");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    if (objects.size() > 0) {
+                        for (final ParseObject object : objects) {
+                            //object.getString("username");
+
+
+                            ParseFile parseFile = (ParseFile) object.get("image");
+                            parseFile.getDataInBackground(new GetDataCallback() {
+                                @Override
+                                public void done(byte[] data, ParseException e) {
+                                    if (e == null && data != null) {
+
+                                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+                                        userimageFromParse.add(bitmap);
+                                        usernamesFromParse.add(object.getString("username"));
+                                        usercommentsFromParse.add(object.getString("comment"));
+
+                                        postClass.notifyDataSetChanged();
+                                    }
+                                }
+                            });
+
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/cerenerdem/instagramcloneparseexample/UploadActivity.java
+++ app/src/main/java/com/cerenerdem/instagramcloneparseexample/UploadActivity.java
@@ -1,18 +1,38 @@
 package com.cerenerdem.instagramcloneparseexample;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class UploadActivity extends AppCompatActivity {
 
     ImageView fotografyukle;
     EditText aciklama;
     Button butonfotografyukle;
-
+    Bitmap secilenfotograf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +48,83 @@ public class UploadActivity extends AppCompatActivity {
     public void calistir_fotografyukle (View view){
 
 
+        String aciklamaalani = aciklama.getText().toString();
+
+        //fotografı sıkıştırıyorum.
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        secilenfotograf.compress(Bitmap.CompressFormat.PNG,50,byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+
+
+        ParseFile parseFile = new ParseFile("image.png",bytes);
+
+
+        ParseObject object = new ParseObject("Posts");
+        object.put("image",parseFile);
+        object.put("aciklama",aciklamaalani);
+        object.put("username", ParseUser.getCurrentUser().getUsername());
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Kaydınız Alındı", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(),FeedActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        });
+    }
+
+
+
+    //Imageview a tıkladığımızda çalışacak.
+    public void calistir_fotografsec (View view){
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},2);
+        } else {
+
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,1);
+
+        }
+
+    }
+
+    //galeriden fotoğraf seçmek için izin almamız gerek.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == 2){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+
+    //izin verildi mi? bilgisi döndüğünde yapılacak işlemlerin kodlaması...
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null){
+
+            Uri uri = data.getData();
+            try {
+                secilenfotograf = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
+                fotografyukle.setImageBitmap(secilenfotograf);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
